6a74c676db1c28be03ecf607af3bdd94d4811c17
SKomarov <Komarov.Sergey.sk@yandex.ru>
2023-12-26 13:49:00 +0300

    update fail Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
+// using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 // коментарий однострочный
 // a = 25, b = 5 => да
 // a = 2, b = 10 => нет
@@ -10,13 +10,30 @@ Console.WriteLine("Hello, World!");
 
 // тип_данных имя_переменной  = значение;
 // camelCase: fitstNumber, secondNumber;
-int number = 9; // number = 9
-int result = number * number; // 9*9 =81
+// int number = 9; // number = 9
+// int result = number * number; // 9*9 =81
 
 // Console.ReadLine() - получить информацию из строчки
 // Console.Write("Введите первое число: ");
 // int firstNumber = Convert.ToInt32(Console.ReadLine());
 
+// Console.Write("Введите число: ");
+// int input = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(input + 1000);
+
+
 Console.Write("Введите число: ");
-int input = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(input + 1000);
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber == secondNumber*secondNumber)
+{
+    Console.WriteLine("Да, " + firstNumber  + " является кватратом от числа " + secondNumber);
+
+}
+else
+{
+    Console.WriteLine("НET, " + firstNumber + " не является кватратом от числа " + secondNumber);
+
+}
