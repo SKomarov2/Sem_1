8fa5f49de8e06e3e7d99631a464b3d0d3c6bd224
SKomarov <Komarov.Sergey.sk@yandex.ru>
2023-12-26 12:58:15 +0300

    addet all files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// коментарий однострочный
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+ // тип_данных имя_переменной  = значение;
+ // camelCase: fitstNumber, secondNumber;
+  int number = 9; // number = 9
+  int result = number * number; // 9*9 =81
