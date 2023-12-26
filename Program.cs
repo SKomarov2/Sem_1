f22512b552202fd28a7f2b2a300d767496c2a4b5
Skomarov <Serega@SegaPC>
2023-12-27 01:31:12 +0300

    update fail Program

--- Program.cs
+++ Program.cs
@@ -46,14 +46,75 @@
 // 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
 // 2 => -2, -1, 0, 1, 2
 
-Console.Write("Введите чсло N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int negativeN = N * -1;
-while (negativeN <= N)
+// Console.Write("Введите чсло N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N < 0)
+// {
+//     //Console.WriteLine("Введено отрецательное чесло");
+//     N = N * -1;
+// }
+// int negativeN = N * -1;
+// while (negativeN <= N)
+// {
+//     Console.Write(negativeN + " ");
+//     negativeN++;
+// }
+
+// int a = 200;
+// int b = 80;
+// int c = 60;
+// int max = a;
+//       if(b > max)
+//       {
+//         max = b;
+//       }
+//       if(c > max)
+//       {
+//         max = c;
+//       }
+// Console.WriteLine(max);
+
+//   if(firstNumber < secondNumber)
+//       {
+//         Console.WriteLine("Первое число " + "`" + firstNumber + "`" + " меньше чем второе число " + "`" + secondNumber + "`");
+//       }
+//        else
+//       {
+//         Console.WriteLine("Введенные числа равны " + "`" + firstNumber + "`");
+//       }
+
+//           if ((a > b) && (a > c)) return a;
+//     else if ((b > a && (b > c )) return b;
+//     else return c; 
+
+// int number = 5;
+
+// if((number % 2) == 0)
+//       {
+//           Console.WriteLine($"Число `{number}` чётное");
+//       }
+// else
+//       {
+//           Console.WriteLine($"Число `{number}` нечётное");
+//       }
+
+using System.Security.Cryptography.X509Certificates;
+
+int number = -40;
+int even=0;
+if (number < 0)
 {
-    Console.Write(negativeN + " ");
-    negativeN++;
+    even=even+number+2;
+    number=number *-1;
 }
-
-
+else
+{
+    even=2;
+}
+while(even <= number) 
+      {
+          Console.Write(even +"\t");
+          even=even+2;
+      }
+       
 
