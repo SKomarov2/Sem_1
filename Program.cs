b64577c3cd04ffd41395cab115a1d15e0694df15
SKomarov <Komarov.Sergey.sk@yandex.ru>
2023-12-26 13:20:47 +0300

    update fail Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
 // коментарий однострочный
 // a = 25, b = 5 => да
@@ -6,7 +8,15 @@ Console.WriteLine("Hello, World!");
 // a = 9, b = -3 => да
 // a = -3, b = 9 => нет
 
- // тип_данных имя_переменной  = значение;
- // camelCase: fitstNumber, secondNumber;
-  int number = 9; // number = 9
-  int result = number * number; // 9*9 =81
+// тип_данных имя_переменной  = значение;
+// camelCase: fitstNumber, secondNumber;
+int number = 9; // number = 9
+int result = number * number; // 9*9 =81
+
+// Console.ReadLine() - получить информацию из строчки
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int input = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(input + 1000);
